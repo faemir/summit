a6ce3c9087e6ad07e791167148003cbd0ff03111
explosivose <explosivose@gmail.com>
2013-07-19 17:06:07 +0100

    Initial ControlScheme commit

--- Assets/scripts/ControlScheme.cs
+++ Assets/scripts/ControlScheme.cs
@@ -3,22 +3,38 @@ using System.Collections;
 
 public class ControlScheme : MonoBehaviour 
 {
+	// Reference to limbs
 	public Transform LeftHand;
 	public Transform RightHand;
 	public Transform LeftFoot;
 	public Transform RightFoot;
 	
+	// Reference to scripts on the limbs
 	private Hand LH;
 	private Hand RH;
+	private Hand LF;
+	private Hand RF;
 	
 	void Start () 
 	{
-	
+		LH = LeftHand.GetComponent<Hand>();
+		RH = RightHand.GetComponent<Hand>();
 	}
 	
 
 	void Update ()
 	{
-	
+		if ( Input.GetButton("GripLeftHand") )
+			;// LH.Grip();
+		
+		if ( Input.GetButton("GripRightHand") )
+			;// RH.Grip();
+		
+		if ( Input.GetButton("GripLeftFoot") )
+			;// LF.Grip();
+		
+		if ( Input.GetButton("GripRightFoot") )
+			;// RF.Grip();
+		
 	}
 }
