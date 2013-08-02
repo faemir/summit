1c670064c7b6c168755f7ef1a3f79c650b15ae66
Daniel Cohen <analoguecolour@gmail.com>
2013-08-02 21:29:19 +0100

    gpl license included and license fix

--- Assets/scripts/Limb.cs
+++ Assets/scripts/Limb.cs
@@ -3,11 +3,11 @@
  * Copyright (C) 2013 Daniel Cohen <analoguecolour@gmail.com>
  *
  * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
+ * modify it under the terms of the GNU General Public
  * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
+ * version 3 of the License, or (at your option) any later version.
  *
- * You should have received a copy of the GNU Lesser General Public
+ * You should have received a copy of the GNU General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
  */
