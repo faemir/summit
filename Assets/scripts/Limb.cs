44a3de7c9c9746bd0c83f9a08b5ba5d770f95b95
Daniel Cohen <analoguecolour@gmail.com>
2013-08-02 21:37:17 +0100

    license rewording

--- Assets/scripts/Limb.cs
+++ Assets/scripts/Limb.cs
@@ -2,13 +2,13 @@
  *
  * Copyright (C) 2013 Daniel Cohen <analoguecolour@gmail.com>
  *
- * This library is free software; you can redistribute it and/or
+ * This script is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public
  * License as published by the Free Software Foundation; either
  * version 3 of the License, or (at your option) any later version.
  *
  * You should have received a copy of the GNU General Public
- * License along with this library; if not, write to the Free Software
+ * License along with this script; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
